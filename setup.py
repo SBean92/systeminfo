b54ed5c321d1ae2f9d5267f6fbe1a8b7e46cc3d3
SBean92 <sean.behan@ucdconnect.ie>
2018-02-23 11:07:04 +0000

    Commiting Changes to git repository

--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-    Setup file for systeminfo.
+'''
+Setup File for systeminfo
+Created on 20th Feb 2018 in Visual Studio Code
 
-    This file was generated with PyScaffold 3.0.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: http://pyscaffold.org/
-"""
-
-import sys
+@author: SBean
+'''
 from setuptools import setup
 
-# Add here console scripts and other entry points in ini-style format
-entry_points = """
-[console_scripts]
-# script_name = systeminfo.module:function
-# For example:
-# fibonacci = systeminfo.skeleton:run
-"""
-
-
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-          entry_points=entry_points,
-          use_pyscaffold=True)
-
-
-if __name__ == "__main__":
-    setup_package()
+setup(
+    name = "systeminfo",
+    version = "0.1",
+    description = "Basic system information for COMP30670",
+    url="https://github.com/SBean92/systeminfo",
+    author = "Sean Behan",
+    aurhor_email = "sean.behan@ucdconnect.ie",
+    licence = "GPL3",
+    packages = ['systeminfo'],
+    entry_points = {
+        'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+    }
+)
---
+++ src/systeminfo/main.py
@@ -0,0 +1,14 @@
+'''
+Main function for systeminfo
+Created on 20th Feb 2018 in Visual Studio Code
+
+@author: SBean92
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    main()
